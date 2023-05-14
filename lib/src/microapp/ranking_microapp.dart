a6ac3a2204551d62e076c86cf9f83e6ec3d85635
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-05-14 01:28:21 -0300

    Update ranking_microapp.dart

--- lib/src/microapp/ranking_microapp.dart
+++ lib/src/microapp/ranking_microapp.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:manga_easy_ranking/src/core/services/api_error_service.dart';
+import 'package:manga_easy_ranking/src/features/data/datasources/manga_easy_datasource.dart';
+import 'package:manga_easy_ranking/src/features/data/datasources/manga_easy_datasource_v1.dart';
+import 'package:manga_easy_ranking/src/features/data/mappers/season_dto_mapper.dart';
+import 'package:manga_easy_ranking/src/features/data/repositories/season_repository_v1.dart';
+import 'package:manga_easy_ranking/src/features/domain/repositories/season_repository.dart';
+import 'package:manga_easy_ranking/src/features/domain/usecases/seasons/get_seasons_usecase.dart';
+import 'package:manga_easy_ranking/src/features/domain/usecases/seasons/get_seasons_usecase_v1.dart';
 import 'package:manga_easy_ranking/src/features/presenters/controllers/ranking_controller.dart';
 import 'package:manga_easy_routes/manga_easy_routes.dart';
 import 'package:get_it/get_it.dart';
@@ -13,7 +21,30 @@ class RankingMicroApp extends MicroApp {
 
   @override
   void registerDependencies() {
-    // controllers
+    //service
+    getIt.registerFactory<ApiErrorService>(
+      () => ApiErrorService(),
+    );
+
+    //mappers
+    getIt.registerFactory(() => SeasonDtoMapper());
+
+    //datasources
+    getIt.registerFactory<MangaEasyDataSource>(
+      () => MangaEasyDataSourceV1(getIt(), getIt(), getIt(), getIt()),
+    );
+
+    //repositories
+    getIt.registerFactory<SeasonRepository>(
+      () => SeasonRepositoryV1(getIt(), getIt()),
+    );
+
+    //usecases
+    getIt.registerFactory<GetSeasonsUseCase>(
+      () => GetSeasonsUsecaseV1(getIt()),
+    );
+
+    //controllers
     getIt.registerLazySingleton(
       () => RankingController(),
     );
