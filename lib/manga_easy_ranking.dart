ef8856f898854c408b2978d6bd342f70a66b3ae3
lucas <lucas.cardo123@hotmail.com>
2023-05-15 11:13:33 -0400

    Add export page

--- lib/manga_easy_ranking.dart
+++ lib/manga_easy_ranking.dart
@@ -1,3 +1,6 @@
 library manga_easy_ranking;
 
 export 'src/microapp/ranking_microapp.dart';
+//Exeção para exportar a page por conta do uso na home do app
+//Não exporte a page consulte o nosso Lider tecnico
+export 'src/features/presenters/ui/pages/ranking_page.dart';
